31ccf310ea2b64481a14c1bbbcd5c910306ffa13
Kent Vincent <k.agot.145976.tc@umindanao.edu.ph>
2026-09-04 15:22:52 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,40 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
-import StatCard from "./components/StatCard";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, Pressable } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [score, setScore] = useState(0);
+
+  const addOrder = () => {
+    setScore(score + 1);
+  };
+
+  const minusOrder = () => {
+    setScore(score - 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>"My Custom Dashboard" </Text>
-      <StatCard
-        title="Total Users 💻"
-        value="1,240"
-        bgColor="#fffec5"
-      />
-      <StatCard
-        title="Revenue 💰"
-        value="12,450"
-        bgColor="#eeafce"
-      />
-      <StatCard
-        title="Pending Issues 🌍"
-        value="3"
-        bgColor="#dfacd6"
-      />
-      </View>
+      <Text > ☕ Coffee Orders: {score}</Text>
+
+      <TouchableOpacity onPress={addOrder} style={styles.button}>
+        <Text style={styles.buttonText}>Add 1+</Text>
+      </TouchableOpacity>
+
+      <Pressable onPress={minusOrder} style={styles.button}>
+        <Text style={styles.buttonText}>Decrease -1</Text>
+      </Pressable>
+
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,      
-        backgroundColor: '#773030',
-        padding: 20,
-        paddingTop: 120,    
-    },
-    header: {
-        fontSize: 24,
-        color: '#ffffff',
-        fontWeight: "bold",
-        marginBottom: '#b17d7d',
-        textAlign: 'center', 
-    },          
-});   
+  container: {
+    flex: 1,
+    backgroundColor: '#d1abeb',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 7,
+  },
+  button: {
+    backgroundColor: '#210f64',
+    paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderRadius: 100 ,
+    fontSize: 100,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '600',
+  },
+});
---
+++ app/index.js
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, Pressable } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  const [score, setScore] = useState(0);
+
+  const addOrder = () => {
+    setScore(score + 1);
+  };
+
+  const minusOrder = () => {
+    setScore(score - 1);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text > ☕ Coffee Orders: {score}</Text>
+
+      <TouchableOpacity onPress={addOrder} style={styles.button}>
+        <Text style={styles.buttonText}>1+</Text>
+      </TouchableOpacity>
+
+      <Pressable onPress={minusOrder} style={styles.button}>
+        <Text style={styles.buttonText}>1-</Text>
+      </Pressable>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d1abeb',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 12,
+  },
+  button: {
+    backgroundColor: '#210f64',
+    paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderRadius: 8 ,
+    fontSize: 100,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '600',
+  },
+});
